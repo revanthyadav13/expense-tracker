a7b79cae59df7545e6b215f7206aed6fe30b8bd4
revanthyadav13 <revanthyadav13@gmail.com>
2023-08-18 11:02:06 +0530

    expense tracker app for deployment

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
-
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const Sequelize = require('sequelize');
 const sequelize = require('./util/database');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
+const fs = require('fs');
+
+
 
 
 const UserDetails=require('./models/userDetails');
 const ExpenseDetails=require('./models/expenseDetails');
 const Order=require('./models/orders');
 const ForgotPasswordRequest=require('./models/forgotPasswordRequest');
+const ContentUploaded=require('./models/contentUploaded');
 
 
 const app = express();
 app.use(cors());
 
-const dotenv = require('dotenv');
+
 
 
 
@@ -24,8 +32,15 @@ const purchaseRoutes=require('./routes/purchase')
 const premiumRoutes=require('./routes/premium')
 const forgotPasswordRoutes=require('./routes/forgotPassword')
 
+const accessLogStream=fs.createWriteStream(
+  path.join(__dirname, 'access.log'),
+  {flags:'a'}
+  );
 
+app.use(helmet());
 app.use(express.json());
+app.use(compression());
+app.use(morgan('combined',{stream:accessLogStream}));
 
 app.use('/user',userDetailsRoutes);
 app.use('/expense',expenseDetailsRoutes);
@@ -39,12 +54,16 @@ ExpenseDetails.belongsTo(UserDetails,{ foreignKey: 'userId' });
 UserDetails.hasMany(Order,{ foreignKey: 'userId' });
 Order.belongsTo(UserDetails,{ foreignKey: 'userId' });
 
-UserDetails.hasMany(ForgotPasswordRequest, { foreignKey: 'userId' });
-ForgotPasswordRequest.belongsTo(UserDetails, { foreignKey: 'userId' });
+UserDetails.hasMany(ForgotPasswordRequest, {foreignKey: 'userId'});
+ForgotPasswordRequest.belongsTo(UserDetails, {foreignKey: 'userId'});
+
+UserDetails.hasMany(ContentUploaded, {foreignKey: 'userId'});
+ContentUploaded.belongsTo(UserDetails, {foreignKey: 'userId'});
+
 
 sequelize
-//.sync({ force: true })
- .sync()
+//.sync({ alter: true })
+.sync()
   .then(result => {
-   app.listen(3000);
+   app.listen(process.env.PORT || 3000);
   })
